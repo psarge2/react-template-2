e30d4d5fe10e46558c5195400ad0637cca631da7
psarge2 <54369285+psarge2@users.noreply.github.com>
2020-12-01 18:34:54 -0500

    added navbar.css, button.js and button.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>AYE!</h1>
-    </div>
+    <>
+    <Router>
+     <Navbar />
+     <Switch>
+       <Route path="/" exact />
+     </Switch>
+     </Router>
+    </>
   );
 }
 
