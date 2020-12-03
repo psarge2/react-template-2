97b0a141d1b7c0d504611a9f6a1a0749221f8378
psarge2 <54369285+psarge2@users.noreply.github.com>
2020-12-03 16:03:17 -0500

    added styles to footer.css modified footer.js syntax

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -22,14 +22,14 @@ function Footer() {
        </section>
        <div className='footer-links'>
         <div className='footer-link-wrapper'>
-          <div className='footer-link-itmes'>
+          <div className='footer-link-items'>
             <h2>About Us</h2>
             <Link to='/sign-up'>How it works</Link>
             <Link to='/'>Testimonials</Link>
             <Link to='/'>Careers</Link>
             <Link to='/'>Terms of Service</Link>
           </div>
-          <div className='footer-link-itmes'>
+          <div className='footer-link-items'>
             <h2>Contact Us</h2>
             <Link to='/'>Contact</Link>
             <Link to='/'>Support</Link>
@@ -38,14 +38,14 @@ function Footer() {
           </div>
         </div>
         <div className='footer-link-wrapper'>
-          <div className='footer-link-itmes'>
+          <div className='footer-link-items'>
             <h2>Videos</h2>
             <Link to='/'>Submit Video</Link>
             <Link to='/'>Ambassadors</Link>
             <Link to='/'>Agency</Link>
             <Link to='/'>Influencer</Link>
           </div>
-          <div className='footer-link-itmes'>
+          <div className='footer-link-items'>
             <h2>Social Media</h2>
             <Link to='/'>Facebook</Link>
             <Link to='/'>Youtube</Link>
