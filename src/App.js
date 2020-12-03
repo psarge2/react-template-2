1d60f8e5cd49c29d92d1590435648d4ba3767b7c
psarge2 <54369285+psarge2@users.noreply.github.com>
2020-12-03 10:48:44 -0500

    added images to components, added background images urls to app.css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -9,8 +12,10 @@ function App() {
     <Router>
      <Navbar />
      <Switch>
-       <Route path='/' exact component=
-       {Home} />
+       <Route path='/' exact component={Home} />
+       <Route path='/services' exact component={Services} />
+       <Route path='/products' exact component={Products} />
+       <Route path='/sign-up' exact component={SignUp} />
      </Switch>
      </Router>
     </>
