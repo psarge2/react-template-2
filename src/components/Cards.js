ee6167c4769712b123035e67aa04237c6bba0b2a
psarge2 <54369285+psarge2@users.noreply.github.com>
2020-12-03 10:09:40 -0500

    added cards to cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -17,9 +17,29 @@ function Cards() {
               />
               <CardsItem 
               src="images/img-2.jpg"
-              text="Explore the hidden waterfall deep inside the Amazon Jungle"
-              label='Adventure'
+              text="Travel through the Islands of Bali in a Private Cruise"
+              label='Luxury'
               path='/services'
+              />   
+            </ul>
+            <ul className="cards__items">
+              <CardsItem 
+              src="images/img-3.jpg"
+              text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
+              label='Mystery'
+              path='/services'
+              />
+              <CardsItem 
+              src="images/img-4.jpg"
+              text="Experience Football on Top of the Himilayan Mountains"
+              label='Adventure'
+              path='/products'
+              />
+              <CardsItem 
+              src="images/img-8.jpg"
+              text="Ride through the Sahara Desert on a guided camel tour"
+              label='Adrenaline'
+              path='/sign-up'
               />   
             </ul>
           </div>
